3bf2060f36b232edf74427c8e815cbc743bc3e26
suissitakwa <suissitakwa@gmail.com>
2024-12-10 15:06:24 -0800

    first commit

---
+++ book-club/src/main/java/com/booksocial/book/BookClubApiApplication.java
@@ -0,0 +1,13 @@
+package com.booksocial.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookClubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookClubApiApplication.class, args);
+	}
+
+}
---
+++ book-club/src/test/java/com/booksocial/book/BookClubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksocial.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookClubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
